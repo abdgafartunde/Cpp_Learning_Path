fa5806b64e82526dfa6cfa46a290301928456754
Abd'gafar Tunde Tiamiyu <abdgafartunde@gmail.com>
2025-09-20 02:44:17 +0800

    updates

--- Basics/hello.cpp
+++ Basics/hello.cpp
@@ -6,7 +6,8 @@ int main() {
     std::cout << "Hello, World! BAE" << std::endl;
     std::cout << "This project uses C++14 standard." << std::endl;
     std::cout << "This is a simple C++ program." << std::endl;
-    
+    std::cerr << "This is an error message." << std::endl;
+    std::clog << "This is a log message." << std::endl;
     
     return 0;
 }
--- code_blk.cpp
+++ code_blk.cpp
@@ -3,7 +3,22 @@
 int main(){
     int a;
     std::cout<< "Welcome \n";
+    std::cout<< "Enter a number: ";
     std::cin >> a;
     std::cout<< "You entered: " << a;
+
+    int b {23};
+    std::cout<< "\nThe value of b is: " << b << std::endl;
+
+    int c(45);
+    std::cout<< "The value of c is: " << c << std::endl;
+
+    std::cout<< "size of int: " << sizeof(int) << " bytes" << std::endl;
+    std::cout<< "size of char: " << sizeof(char) << " bytes" << std::endl;
+    std::cout<< "size of bool: " << sizeof(bool) << " bytes" << std::endl;
+    std::cout<< "size of float: " << sizeof(float) << " bytes" << std::endl;
+    std::cout<< "size of double: " << sizeof(double) << " bytes" << std::endl;
+    
+    
     return 0;
 }
